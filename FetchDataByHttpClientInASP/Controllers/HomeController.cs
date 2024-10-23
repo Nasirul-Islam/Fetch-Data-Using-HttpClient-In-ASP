2862df0f8943cb6f6d1cea03b477b14ba832afba
Nasirul-Islam <nasirulislam@gmail.com>
2024-10-23 23:53:10 +0600

    load data in server side & pass to view page & show

---
+++ FetchDataByHttpClientInASP/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+using FetchDataByHttpClientInASP.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+
+namespace FetchDataByHttpClientInASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, HttpClient httpClient)
+        {
+            _logger = logger;
+            _httpClient = httpClient;
+        }
+        //[HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            string apiUrl = "https://jsonplaceholder.typicode.com/todos/1";
+            var response = await _httpClient.GetAsync(apiUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var todoItem = JsonConvert.DeserializeObject<TodoItem>(jsonData);
+                return View(todoItem);
+            }
+            return View(null);
+        }
+        [HttpGet]
+        public async Task<IActionResult> LoadUsers() 
+        {
+            string apiUrl = "https://jsonplaceholder.typicode.com/users";
+            var response = await _httpClient.GetAsync($"{apiUrl}/users");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var users = JsonConvert.DeserializeObject(jsonData);
+                return Json(users);
+                //var users = JsonConvert.DeserializeObject<List<User>>(jsonData);
+                //return Json(users); // Return data as JSON
+            }
+            return StatusCode(500, "Error fetching data");
+            //return View(null);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FetchDataByHttpClientInASP/Models/ModelClass.cs
@@ -0,0 +1,36 @@
+﻿namespace FetchDataByHttpClientInASP.Models
+{
+    public class ModelClass
+    {
+    }
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public bool Completed { get; set; }
+    }
+    public class Geo
+    {
+        public string? Lat { get; set; }
+        public string? Lng { get; set; }
+    }
+
+    public class Address
+    {
+        public string? Street { get; set; }
+        public string? Suite { get; set; }
+        public string? City { get; set; }
+        public string? Zipcode { get; set; }
+        public Geo? Geo { get; set; }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public Address? Address { get; set; }
+    }
+
+}
